c5eb1579f52cd39d12c65e11e52f622da436ad94
IssaDia <44264590+IssaDia@users.noreply.github.com>
2023-11-05 17:33:13 +0100

    e2e test file change

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -2,7 +2,7 @@ const user = {
   email: "test@cypress.com",
   password: "test-cypress",
 };
-describe("Example Test", () => {
+describe("Homepage", () => {
   it("displays the right number elements when filtering by price", () => {
     cy.visit("/");
     cy.get('[data-test="product-card"]').should("have.length", 12);
@@ -18,21 +18,5 @@ describe("Example Test", () => {
     cy.get('[data-test="add-product-cart-btn"]').first().click();
     cy.wait(3000);
     cy.get('[data-test="cart-quantity"]').should("have.text", "(1)");
-  });
-  it("can register successfully", () => {
-    cy.visit("/register");
-    cy.get('input[name="email"]').type(user.email);
-    cy.get('input[name="password"]').type(user.password);
-    cy.get('input[name="confirmPassword"]').type(user.password);
-    cy.get('input[name="agreeTerms"]').click({ force: true });
-    cy.get('button[type="submit"]').click({ force: true });
-    cy.url().should("eq", `${Cypress.config().baseUrl}/expected-page`);
-  });
-  it("can login successfully", () => {
-    cy.visit("/login");
-    cy.get('input[name="email"]').type(user.email);
-    cy.get('input[name="password"]').type(user.password);
-    cy.get('button[type="submit"]').click({ force: true });
-    cy.url().should("eq", `${Cypress.config().baseUrl}/`);
   });
 });
